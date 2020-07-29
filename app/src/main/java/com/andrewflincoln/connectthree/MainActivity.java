9613f0a555d13dc3647dbd02562d4d80e7b0079f
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-07-29 14:52:39 -0700

    red yellow alternate

--- app/src/main/java/com/andrewflincoln/connectthree/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/connectthree/MainActivity.java
@@ -3,8 +3,36 @@ package com.andrewflincoln.connectthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int activePlayer = 0;
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+        int currentTag = Integer.parseInt(counter.getTag().toString());
+        gameState[currentTag] = activePlayer;
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+
+
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+
+        }
+        counter.setTranslationY(-1500);
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
